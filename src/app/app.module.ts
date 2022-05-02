3f36c387c215ad76f93d55e32e909a8ea97d741b
Victor Bonanho - Desenvolvedor - SoftVision <victor.timotti@softvision.com.br>
2022-05-02 09:13:30 -0300

    to preload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { CarouselComponent } from './components/carousel/carousel.component';
 import { OwlCarouselComponent } from './components/owl-carousel/owl-carousel.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
@@ -41,6 +42,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CampoControlErroComponent,
     CarouselComponent,
     OwlCarouselComponent,
+    LoaderComponent,
   ],
   imports: [
     BrowserModule,
